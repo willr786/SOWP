4ba9e93787f1d293e9fdc589d2351382c024e412
willr786 <willr_786@yahoo.com>
2022-07-18 07:37:55 -0400

    More python files.

---
+++ CH02/dollar_display.py
@@ -0,0 +1,5 @@
+# This program demonstrates how a floating-point
+# number can be displayed as currency.  
+monthly_pay = 5000.0
+annual_pay =  monthly_pay * 12.0
+print(f'The monthly payment is ${annual_pay:,.2f}')
