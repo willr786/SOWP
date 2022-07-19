d4d8740c01874fd91c5e7d4206fb71d8bb863bd4
willr786 <willr_786@yahoo.com>
2022-07-19 09:18:59 -0400

    Chapter 3 files

---
+++ CH03/test_average.py
@@ -0,0 +1,24 @@
+# This progam gets three test scores and displays
+# their average.  It congratulates teh user if the 
+# average is a high score.  
+
+# The HIGH_SCORE named constant holds the value that is
+# considered a high score.  
+HIGH_SCORE = 95
+
+# Get the three test scores.
+test1 = int(input('Enter the score for test 1: '))
+test2 = int(input('Enter the score for test 2: '))
+test3 = int(input('Enter the score for test 3: '))
+
+# Calculate the average test score.
+average = (test1 + test2 + test3) / 3
+
+# Print the average.
+print(f'The average score is {average:.1f}.')
+
+# If the average is a high score,
+# congratulate the user.
+if average >= HIGH_SCORE:
+    print('Congratulations!')
+    print('That is a great average!')
