0514bf35cc60097191bef924b9f26027b162a3d2
willr786 <willr_786@yahoo.com>
2022-07-22 07:50:40 -0400

    First CH04 files.

---
+++ CH04/temperature.py
@@ -0,0 +1,23 @@
+# This program assistes a technician in the process 
+# of checking a substance's temperature.
+
+# Named constant to represent the maximum
+# temperature.
+MAX_TEMP = 102.5
+
+# Get the substance's temperature.
+temperature = float(input("Enter the substanc's Cessius temperature: "))
+
+# As long as necessary, instruct the user to 
+# adjust the thermostat.
+while temperature > MAX_TEMP:
+    print('The temperature is too high.')
+    print('Turn the thermostat down and wait')
+    print('5 minutes.  Then take the temperature')
+    print('again and enter it.')
+    temperature = float(input('Enter the new Celsius temperature: '))
+
+# Reming the user to check the temperature again 
+# in 15 minutes.
+print('The temperature is acceptable.')
+print('Check it again in 15 minutes.')
